01c9a4a34ae40691c80bc2f18457261384e8f919
Artur <artur.zajaczkowski76@gmail.com>
2022-04-26 09:32:08 +0200

    add form to input expenses

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import './ExpenseItem.css';
 
 const ExpenseItem = (props)=> {
 
+  const [expenseTitle, setExpenseTitle] = useState(props.title);
+  let title = props.title;
+
+  const clickHandler = ()=>{
+    setExpenseTitle(`Buy present for wife :-)`)
+  }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
 
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{expenseTitle}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler} >Change Title</button>
     </Card>
   );
 }
